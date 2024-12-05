00b416b686c310792583dfca8f4a160029043399
Tuan2245 <daganhtuan2245@gmail.com>
2024-12-05 10:24:33 +0700

    Add View Category Admin

---
+++ EprojectSem3/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EprojectSem3.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
