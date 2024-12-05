2f3efc67bc9e79ca6bc96752d2c0ef0667e34ea7
LyPahn <91794300+LyPahn@users.noreply.github.com>
2024-12-05 17:12:18 +0700

    .

--- EprojectSem3/Areas/Admin/Controllers/CategoryController.cs
+++ EprojectSem3/Areas/Admin/Controllers/CategoryController.cs
@@ -1,18 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EprojectSem3.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EprojectSem3.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var categories = _context.Categories.ToList();
+            return View(categories);
         }
 
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
         }
 
         public IActionResult Edit()
