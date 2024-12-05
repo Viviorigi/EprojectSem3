dd0be142f0bb105fe8fc824d1344b380f6ce8c7a
LyPahn <91794300+LyPahn@users.noreply.github.com>
2024-12-06 02:28:17 +0700

    update controller admin category

--- EprojectSem3/Areas/Admin/Controllers/CategoryController.cs
+++ EprojectSem3/Areas/Admin/Controllers/CategoryController.cs
@@ -24,20 +24,64 @@ namespace EprojectSem3.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Category category)
+        public IActionResult Create( Category category)
         {
-            if (!ModelState.IsValid)
+            if (category != null)
             {
                 _context.Categories.Add(category);
                 _context.SaveChanges();
+                ViewBag.message = "Create successful";
+                return RedirectToAction("Index");
+            }
+           
+            return View(category);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var category = _context.Categories.SingleOrDefault(x => x.CategoryId == id);
+            if (category == null)
+            {
+                ViewBag.message = "category does not exist";
                 return RedirectToAction("Index");
             }
             return View(category);
         }
 
-        public IActionResult Edit()
+        [HttpPost]
+        public IActionResult Edit(int id , Category category)
         {
-            return View();
+            var c = _context.Categories.SingleOrDefault(x => x.CategoryId == id);
+            if (c != null)
+            {
+               c.Name = category.Name;
+                c.Description = category.Description;
+                _context.SaveChanges();
+                ViewBag.message = "Update category successful";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var listing = _context.Listings.FirstOrDefault(x => x.CategoryId == id);
+            if (listing == null)
+            {
+                var category = _context.Categories.FirstOrDefault(x => x.CategoryId == id);
+                if (category != null)
+                {
+                    _context.Categories.Remove(category);
+                    _context.SaveChanges();
+                    ViewBag.message = "Delete Category successful";
+                    return RedirectToAction("Index");
+                }
+            }
+            
+                ViewBag.message = "Existing posts cannot be deleted.";
+                return View("index");
+            
+            
         }
     }
 }
--- EprojectSem3/Models/Category.cs
+++ EprojectSem3/Models/Category.cs
@@ -1,10 +1,13 @@
-﻿namespace EprojectSem3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EprojectSem3.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
+        [Required(ErrorMessage ="Category Name is not required")]
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public ICollection<Listing> Listings { get; set; } 
     }
 
