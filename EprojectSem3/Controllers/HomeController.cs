4b0fab9476092af538b63a4c72ad0c2dd29978a8
vivi <duong682@gmailcom>
2024-12-05 16:13:56 +0700

    fix bug connect

--- EprojectSem3/Controllers/HomeController.cs
+++ EprojectSem3/Controllers/HomeController.cs
@@ -7,10 +7,13 @@ namespace EprojectSem3.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -27,6 +30,19 @@ namespace EprojectSem3.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult TestConnection()
+        {
+            try
+            {
+                // Run a simple query to check the connection
+                var regionsCount = _context.Regions.Count();
+                return Content($"Database is connected! Regions count: {regionsCount}");
+            }
+            catch (Exception ex)
+            {
+                return Content($"Database connection failed: {ex.Message}");
+            }
         }
     }
 }
--- EprojectSem3/Program.cs
+++ EprojectSem3/Program.cs
@@ -1,9 +1,17 @@
+using EprojectSem3.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
