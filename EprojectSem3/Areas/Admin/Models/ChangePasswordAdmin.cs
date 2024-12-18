c05c06d595ed273b92b108fd56f51bfdf294104b
vivi <duong682@gmailcom>
2024-12-18 16:59:01 +0700

    change password admin

---
+++ EprojectSem3/Areas/Admin/Models/ChangePasswordAdmin.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Realtors_Portal.Areas.Admin.Models
+{
+    public class ChangePasswordAdmin
+    {
+        [Required(ErrorMessage = "New password is required.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required.")]
+        [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
