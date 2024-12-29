a9e69c8e691cd5b6b801d366b3469837c7bd0ba9
Viviorigi <duong682@gmail.com>
2024-12-30 00:34:02 +0700

    .

--- DataAccessLayer_DAL/Models/Listing.cs
+++ DataAccessLayer_DAL/Models/Listing.cs
@@ -5,11 +5,11 @@ namespace DataAccessLayer_DAL.Models
     public class Listing
     {
         public int ListingId { get; set; }
-        [Required(ErrorMessage ="Title is not required")]
+        [Required(ErrorMessage ="Title is  required")]
         public string Title { get; set; }
-        [Required(ErrorMessage ="Description is not required")]
+        [Required(ErrorMessage ="Description is  required")]
         public string Description { get; set; }
-        [Required(ErrorMessage ="Price is not required")]
+        [Required(ErrorMessage ="Price is  required")]
         public double Price { get; set; }
         //[Required(ErrorMessage = "Picture thumbnail cannot be empty")]
 		public string? Image {  get; set; }
